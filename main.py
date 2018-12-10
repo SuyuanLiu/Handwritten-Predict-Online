19dd191947600a7b24b932934b7268ffc9d67a96
SuyuanLiu <suyuanliu0618@gmail.com>
2018-12-10 21:17:47 +0800

    upload multiple models at one time

--- main.py
+++ main.py
@@ -1,52 +1,42 @@
 import numpy as np 
 import tensorflow as tf 
 from flask import Flask, jsonify, render_template, request 
-
 import model 
 
-# x = tf.placeholder(tf.float32, [None, 784])
-# sess = tf.Session()
 
-# with tf.variable_scope('regression'):
-#     y1 = model.regression(x)
-#     # saver = tf.train.Saver()
-#     saver = tf.train.import_meta_graph('checkpoints/regression/regression.ckpt.meta')
-#     tf.train.latest_checkpoint('checkpoints/regression/regression.ckpt')
-#     # saver.restore(sess, "checkpoints/regression/regression.ckpt")
+class Predict:
+    def __init__(self, model_name, checkpoint_dir):
+        self.graph = tf.Graph()   # create graph for each instance individuly
+        self.model_name = model_name
 
-# with tf.variable_scope('cnn'):
-#     keep_prob = tf.placeholder(tf.float32)
-#     x_image = tf.reshape(x, [-1, 28, 28, 1])
-#     y2 = model.cnn(x_image, keep_prob)
-#     # saver = tf.train.Saver()
-#     saver2 = tf.train.import_meta_graph('checkpoints/cnn/cnn.ckpt.meta')
-#     tf.train.latest_checkpoint('checkpoints/cnn/cnn.ckpt')
-    # saver.restore(sess, "checkpoints/cnn/cnn.ckpt")
+        with self.graph.as_default():
+            self.x = tf.placeholder(tf.float32, [None, 784])
+            self.keep_prob = tf.placeholder(tf.float32)
+            
+            if self.model_name == 'regression':
+                self.output = model.regression(self.x)
+            else:
+                self.output = model.cnn(self.x, self.keep_prob)
+            self.saver = tf.train.Saver()
 
-x = tf.placeholder(tf.float32, [None, 784])
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-keep_prob = tf.placeholder(tf.float32)
-
-sess = tf.Session()
-
-# with tf.variable_scope('regression'):
-y1 = model.regression(x)
-saver = tf.train.Saver()
-ckpt = tf.train.get_checkpoint_state('checkpoints/regression/')
-# if ckpt and ckpt.model_checkpoint_path:
-saver.restore(sess, ckpt.model_checkpoint_path)
-
-# # with tf.variable_scope('cnn'):
-# y2 = model.cnn(x_image, keep_prob)
-# saver = tf.train.Saver()
-# ckpt = tf.train.get_checkpoint_state('checkpoints/cnn/')
-# # if ckpt and ckpt.model_checkpoint_path:
-# saver.restore(sess, ckpt.model_checkpoint_path)
-
+        self.sess = tf.Session(graph = self.graph)
+        with self.graph.as_default():
+            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+        
+    def predict(self, input):
+        if self.model_name == 'regression':
+            return self.sess.run(self.output, feed_dict={self.x:input}).flatten().tolist()
+        else:
+            return self.sess.run(self.output, feed_dict={self.x:input, self.keep_prob:1.0}).flatten().tolist()
+            
 def regression(input):
-    return sess.run(y1, feed_dict={x:input}).flatten().tolist()
+    regression_pre = Predict('regression', 'checkpoints/regression/')
+    return regression_pre.predict(input)
 def cnn(input):
-    return sess.run(y2, feed_dict={x:input, keep_prob:1.0}).flatten().tolist()
+    cnn_pre = Predict('cnn', 'checkpoints/cnn/')
+    return cnn_pre.predict(input)
 
 app = Flask(__name__)
 
@@ -54,9 +44,7 @@ app = Flask(__name__)
 def mnist():
     input = ((255 - np.array(request.json, dtype=np.uint8)) /255.0).reshape([1, 784])
     output1 = regression(input)
-    output2 = output1
-    # output2 = cnn(input)
-    # output1 = output2
+    output2 = cnn(input)
     return jsonify(results=[output1, output2])
 
 @app.route('/')
